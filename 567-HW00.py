3f9552985f68942eb3ce1b98029549ac5f582e00
DivyamshuM <103683889+DivyamshuM@users.noreply.github.com>
2023-01-24 18:20:20 -0500

    Add files via upload

---
+++ 567-HW00.py
@@ -0,0 +1,10 @@
+import datetime
+def my_brand(homework_name):
+    print("=*=*=*= Divyamshu Mandadi =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*= ")
+    print("=*=*=*= The name of the homework assignment: {} =*=*=*= ".format(homework_name))
+    print("=*=*=*= Date and time: {} =*=*=*= ".format(datetime.datetime.now()))
+
+my_brand("HW00-Tools setup")
+print("Hello world")
+my_brand("HW00-Tools setup")
